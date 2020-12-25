8d9b8d5ad14b956187fa79abd5ff2aadf4acef72
Dongha Chung <haetae.dongha@gmail.com>
2020-12-25 15:40:22 +0900

    need to test stuffs

--- test/test.cpp
+++ test/test.cpp
@@ -1,4 +1,22 @@
+#include <iostream>
+
+
 
 int main(){
+
+    //Testing for two surfels
+    //yaw change with same plane measures
+    //state = [x, y, z, roll, pitch, yaw]
+    //measure = [nx, ny, nz, d]
+    //surfel = [nx, ny, nz, x, y, z]
+
+    //Initial state = [0, 0, 0, 0, 0, 0]
+    //Second state = [0, 0, 0, 0, 0, pi/6]
+
+    
+
+
+
+
     return 0;
 }
